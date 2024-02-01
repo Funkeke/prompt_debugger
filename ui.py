2f0abfe2ce09fb7da8506cd8c6d75882553f8ffb
fangkeke <3339698829@qq.com>
2024-02-01 15:59:04 +0800

    package test

--- ui.py
+++ ui.py
@@ -206,13 +206,13 @@ class WinGUI(Tk):
         return label
 
     def __tk_label_qwen_copyright_text(self, parent):
-        label = Label(parent, text="阿里千问大模型测试© 2024", anchor="center", )
-        label.place(relx=0.04, rely=0.94, relwidth=0.42, relheight=0.05)
+        label = Label(parent, text="阿里千问大模型测试©gnway 2024", anchor="center", )
+        label.place(relx=0.00, rely=0.94, relwidth=0.42, relheight=0.05)
         return label
 
     def __tk_label_zhipu_copyright_text(self, parent):
-        label = Label(parent, text="清华智谱大模型测试© 2024", anchor="center", )
-        label.place(relx=0.04, rely=0.94, relwidth=0.42, relheight=0.05)
+        label = Label(parent, text="清华智谱大模型测试©gnway 2024", anchor="center", )
+        label.place(relx=0.00, rely=0.94, relwidth=0.42, relheight=0.05)
         return label
 
     def __tk_label_qwen_top_p_label(self, parent):
